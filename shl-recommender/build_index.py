ba4abbbf21bd0e2899d0ecb3fa555d6874cf1ada
Kartik Mehra <kartik742m@gmail.com>
2026-05-15 01:43:32 +0530

    feat: initialize indexing boilerplate

---
+++ shl-recommender/build_index.py
@@ -0,0 +1,9 @@
+import chromadb
+from sentence_transformers import SentenceTransformer
+
+def build_index():
+    """Build the vector index using ChromaDB."""
+    pass
+
+if __name__ == "__main__":
+    pass
